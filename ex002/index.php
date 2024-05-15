407b479f05aaf33f94e6e3d1b1692b762e20b0f6
Thiago Talpo <166398726+thiagotalpo@users.noreply.github.com>
2024-05-15 08:20:16 -0300

    att

---
+++ ex002/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Exemplo de PHP</h1>
+    <?php
+         
+        echo "Hoje é dia " . date("d");
+        
+    ?>
+</body>
+</html>
---
+++ ex003/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        //0x = Hexadecimal 0b = Binário 0 = Octal
+        var_dump(); // função para mostrar o tipo da variavel
+        $num = (int) "3e2"; //força a variavel para algum tipo primitivo (coerção)
+    ?>
+</body>
+</html>
